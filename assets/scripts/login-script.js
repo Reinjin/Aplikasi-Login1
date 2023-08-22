3cea4e06eb8cc3e0f1789fcb36ed697f9093526d
Reinjin <115994253+Reinjin@users.noreply.github.com>
2023-08-22 23:23:54 +0700

    Update login-script.js
    Untuk JSdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -3,6 +3,7 @@
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+ * Gunakan A untuk A
  */
 
 const loginFormElement = document.querySelector('#loginForm');
